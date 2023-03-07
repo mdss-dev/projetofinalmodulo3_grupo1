0c7a99289e6cc3c668504339627f51ae6c2e2d92
Daphne Cordeiro <daphnecordeiro22@gmail.com>
2023-03-06 22:18:17 -0300

    model produto

--- src/main/java/com/polotech/t924/grupo1/projetofinal/model/Produto.java
+++ src/main/java/com/polotech/t924/grupo1/projetofinal/model/Produto.java
@@ -1,4 +1,22 @@
 package com.polotech.t924.grupo1.projetofinal.model;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Produto {
+    @Id
+    private String id;
+    private String nome;
+    private String marca;
+    private Double preco;
+    private Double quantidade;
+    private LocalDateTime ultimoPedido;
+
 }
